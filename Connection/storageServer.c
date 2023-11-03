593e4648c7bc69280bd192aa21542559fcb48727
Gaurav <gaurav.behera@research.iiit.ac.in>
2023-11-04 00:03:40 +0530

    Added NM request thread in SS

--- Connection/storageServer.c
+++ Connection/storageServer.c
@@ -1,12 +1,29 @@
 #include "headers.h"
 
+void *serveNM_Requests(void *args)
+{
+	int nmfd = *((int *)args);
+	while (1)
+	{
+		char buffer[4096];
+		int bytesRecv = recv(nmfd, buffer, sizeof(buffer), 0);
+		if (bytesRecv == -1)
+		{
+			perror("recv");
+		}
+		printf("RECV: %s\n", buffer);
+	}
+	return NULL;
+}
+
 int main()
 {
 	int namingServerPort = 6969;
 	int sockfd = joinNamingServer("127.0.0.1", namingServerPort, "Storage Server");
-	while(1){
-		
-	}
+
+	pthread_t nmThread;
+	pthread_create(&nmThread, NULL, serveNM_Requests, (void *)&sockfd);
+	pthread_join(nmThread, NULL);
 	return 0;
 }
 
