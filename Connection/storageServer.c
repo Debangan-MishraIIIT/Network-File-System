d6f2fe24bc9b901dab1ba472cab638dd9e4039cf
Debangan-MishraIIIT <debangan.mishra@students.iiit.ac.in>
2023-11-04 01:10:04 +0530

    permissions included

--- Connection/storageServer.c
+++ Connection/storageServer.c
@@ -1,4 +1,5 @@
 #include "headers.h"
+// there should be an inifinite thread in the NS side that also checks and updates the paths
 
 int check_path_exists(const char *directoryPath)
 {
@@ -13,6 +14,20 @@ int check_path_exists(const char *directoryPath)
 	}
 }
 
+void convertPermissions(mode_t st_mode, char *perms) {
+    perms[0] = (S_ISDIR(st_mode)) ? 'd' : '-';
+    perms[1] = (st_mode & S_IRUSR) ? 'r' : '-';
+    perms[2] = (st_mode & S_IWUSR) ? 'w' : '-';
+    perms[3] = (st_mode & S_IXUSR) ? 'x' : '-';
+    perms[4] = (st_mode & S_IRGRP) ? 'r' : '-';
+    perms[5] = (st_mode & S_IWGRP) ? 'w' : '-';
+    perms[6] = (st_mode & S_IXGRP) ? 'x' : '-';
+    perms[7] = (st_mode & S_IROTH) ? 'r' : '-';
+    perms[8] = (st_mode & S_IWOTH) ? 'w' : '-';
+    perms[9] = (st_mode & S_IXOTH) ? 'x' : '-';
+    perms[10] = '\0'; // Null-terminate the string
+}
+
 void *take_inputs_dynamically(void *args)
 {
 	while (1)
@@ -20,9 +35,23 @@ void *take_inputs_dynamically(void *args)
 		char *path;
 		path = malloc(sizeof(char) * 250);
 		scanf("%s", path);
-		if(check_path_exists(path)){
+		if (check_path_exists(path))
+		{
+			struct stat dirStat;
 			printf("path exists\n");
-		}else{
+			int r = stat(path, &dirStat);
+			if (r == -1)
+			{
+				fprintf(stderr, "File error\n");
+				exit(1);
+			}
+			char perms[11];
+			convertPermissions(dirStat.st_mode, perms);
+			printf("Permission bits: %s\n", perms);
+			// send path to NS
+		}
+		else
+		{
 			printf("path does not exist\n");
 		}
 	}
@@ -48,16 +77,16 @@ void *serveNM_Requests(void *args)
 int main()
 {
 	int namingServerPort = 6969;
-	int sockfd = joinNamingServer("127.0.0.1", namingServerPort, "Storage Server");
+	// int sockfd = joinNamingServer("127.0.0.1", namingServerPort, "Storage Server");
 
 	pthread_t nmThread;
 	pthread_t inputThread;
 
-	pthread_create(&nmThread, NULL, serveNM_Requests, (void *)&sockfd);
-	pthread_join(nmThread, NULL);
-
+	// pthread_create(&nmThread, NULL, serveNM_Requests, (void *)&sockfd);
 	pthread_create(&inputThread, NULL, take_inputs_dynamically, NULL);
 	pthread_join(inputThread, NULL);
+	// pthread_join(nmThread, NULL);
+
 	return 0;
 }
 
