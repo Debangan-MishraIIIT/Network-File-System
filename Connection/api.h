699b13f8bc7d603b3a226275e9487d5d0eccb708
Debangan-MishraIIIT <debangan.mishra@students.iiit.ac.in>
2023-11-13 23:27:18 +0530

    changes to file functionality

--- Connection/api.h
+++ Connection/api.h
@@ -9,5 +9,7 @@ int recursive_directory_deletion(char *curr_address);
 int remove_files_and_directory(char* path);
 void get_request(char request[], char actual_request[]);
 void get_path(char request[], char path[]);
+int handle_naming_server_commands(char* command, char* inputS);
+void parse_input(char* array[], char* inputS);
 
 #endif
